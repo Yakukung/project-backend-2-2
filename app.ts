a14f2bc94ee6d45629a45077935c70bb244cea35
อัษฎาวุธ ไชยรักษ์ <yakukung@MacBookAir-2.local>
2024-03-12 01:27:08 +0700

    Update all files

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-//app.ts
 import express from "express";
 import cors from "cors";
 import path from "path";
@@ -10,19 +9,21 @@ import { router as ranking } from "./api/facemash-ranking";
 export const app = express();
 const PORT = 3000;
 
-
-
 app.use(
   cors({
-    origin: "*",
+    origin: "*", // or specify the allowed origin(s) explicitly
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // specify the allowed methods
+    credentials: true, // enable credentials (if needed)
+    optionsSuccessStatus: 204, // for preflight requests
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/", index);
 app.use("/facemash", facemash);
 app.use("/facemash/ranking", ranking);
 
-
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
