b910ea85280a6371b2f20b4cc9f8dee5492f9015
อัษฎาวุธ ไชยรักษ์ <yakukung@MacBookAir-2.local>
2024-03-15 23:24:11 +0700

    Update all files

--- dbconnect.ts
+++ dbconnect.ts
@@ -1,17 +1,17 @@
 import mysql from "mysql";
 import util from "util";
 export const conn = mysql.createPool({
-  // connectionLimit: 10,
-  // host: "202.28.34.197",
-  // user: "web66_65011212083",
-  // password: "65011212083@csmsu",
-  // database: "web66_65011212083",
-
   connectionLimit: 10,
-  host: "localhost",
-  user: "web_work5",
-  password: "",
-  database: "web_work5",
+  host: "202.28.34.197",
+  user: "web66_65011212083",
+  password: "65011212083@csmsu",
+  database: "web66_65011212083",
+
+  // connectionLimit: 10,
+  // host: "localhost",
+  // user: "web_work5",
+  // password: "",
+  // database: "web_work5",
 
   
 });
